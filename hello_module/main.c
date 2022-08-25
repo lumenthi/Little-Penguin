7949064b7538796ed1544eb8ac8a69b2072b590b
lumenthi <lumenthi@student.42.fr>
2022-08-25 12:54:02 +0200

    :art: Hello module fixes

--- hello_module/main.c
+++ hello_module/main.c
@@ -1,13 +1,15 @@
-#include <linux/module.h> // Needed by modules
-#include <linux/kernel.h> // KERN_INFO
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-int init_module(void) {
-	printk(KERN_INFO "HELLO\n");
+int init_module(void)
+{
+	printk(KERN_DEBUG "Hello world !\n");
 	return 0;
 }
 
-void cleanup_module(void) {
-	printk(KERN_INFO "GOODBYE\n");
+void cleanup_module(void)
+{
+	printk(KERN_DEBUG "Cleaning up module.\n");
 }
 
 MODULE_LICENSE("GPL");
