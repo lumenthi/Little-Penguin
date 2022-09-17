cb5d0c89c57233bb6b51b3f05467929799beb84e
lumenthi <lumenthi@e1r10p2.clusters.42paris.fr>
2022-09-17 15:29:53 +0200

    :fire: Removing files for defense

--- assignment-07/read_loop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-while true
-do
-cat /sys/kernel/debug/fortytwo/foo
-done
--- assignment-07/write_loop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-while true
-do
-sudo sh -c 'echo "awesome data"' > /sys/kernel/debug/fortytwo/foo
-done
